c9fb7eb9d277b3374f6199a6b3bcf508cdbb4eca
randall ishino <rishino24@gmail.com>
2017-12-31 00:31:47 -0800

    js added

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+var questions = [ {
+    question: "Which team holds the best regular season record in NBA history?",
+    choices: ["16 Warriors", "96 Bulls", "72 Lakers", "97 Bulls"],
+    answer: "16 Warriors"
+}, {
+    question: "Which player has the most NBA championships?",
+    choices: ["Michael Jordan", "Bill Russell", "Sam Jones", "Magic Johnson"],
+    answer: "Bill Russell"
+}, {
+    question: "Who holds the record for the 2nd most points scored in a single game?",
+    choices: ["Wilt Chamberlain", "Michael Jordan", "Kobe Bryant", "Elgin Baylor"],
+    answer: "Kobe Bryant"
+}, {
+    question: "Which team had the longest winning streak in NBA history?",
+    choices: ["Golden State Warriors", "Chicago Bulls", "Los Angeles Lakers", "Miami Heat"],
+    answer:"Los Angeles Lakers"
+}, {
+    question: "How many MVP awards does LeBron James have?",
+    choices: ["3", "4", "2", "5"],
+    answer: "4"
+}, {
+    question: "Who recently averaged a triple-double over an NBA season?",
+    choices: ["James Harden", "Russell Westbrook", "Draymond Green", "LeBron James"],
+    answer: "Russell Westbrook"
+}, {
+    question: "What is the NBA record for most rebounds in a single game?",
+    choices: ["55", "51", "45", "49"],
+    answer: "55"
+}, {
+    question: "In recent memory, who is widely considered the greatest shooter of all-time?",
+    choices: ["Ray Allen","Drazen Petrovic", "Stephen Curry", "Reggie Miller"],
+    answer: "Stephen Curry"
+},
+]
+
+var timer = 200;
+var timerId;
+var correct = 0;
+var incorrect = 0;
